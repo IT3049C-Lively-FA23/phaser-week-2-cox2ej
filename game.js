df5f998d459db9018d0eec9ec28f4b995addc719
Ethan Cox <cox2ej@mail.uc.edu>
2024-03-23 17:28:11 -0400

    Added ground

--- game.js
+++ game.js
@@ -11,6 +11,17 @@ const game = new Phaser.Game(800,600, Phaser.AUTO, '', {
         game.load.spritesheet('woof', 'assets/woof.png', 32, 32)
     }
     function create(){
+        game.physics.startSystem(Phaser.Physics.ARCADE)
+
+        game.add.sprite(0,0,'sky')
+
+        platforms = game.add.group()
+        platforms.enableBody = true
+
+        let ground = platforms.create(0, game.world.height - 64, 'ground')
+        ground.scale.setTo(2,2)
+        ground.body.immovable
+
         
     }
     function update(){
